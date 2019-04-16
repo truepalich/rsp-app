18ba4e527b880b7037b81ad3a15566017010ce5a
pavel_lebedev <designer.lebedev@gmail.com>
2019-04-16 20:24:57 +0300

    initial commit

--- routes.js
+++ routes.js
@@ -2,10 +2,12 @@ const getList = require('./app/lists/getList');
 const createListItem = require('./app/lists/createListItem');
 const getListItem = require('./app/lists/getListItem');
 const editListItem = require('./app/lists/editListItem');
+/*-----------------------*/
 
 module.exports = (app, collection) => {
   getList(app, collection);
   createListItem(app, collection);
   getListItem(app, collection);
   editListItem(app, collection);
+  /*-----------------------*/
 };
