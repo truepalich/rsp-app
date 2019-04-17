b0e59416512f1e20b50c3906538a024a7ed3383d
Stanislav Tytarenko <civictyper13@gmail.com>
2019-04-17 22:54:15 +0300

    Edit note logic added

--- app/notes/editNote.js
+++ app/notes/editNote.js
@@ -3,10 +3,9 @@ const ObjectId = require('mongodb').ObjectId;
 module.exports = function(app, collection) {
     app.put('/api/notes/:id', async (req, res) => {
         let query = {_id: ObjectId(req.params.id)};
-        let infoForUpd = {title: 'New title333333'};
 
         try {
-            let result = await collection.updateOne(query, {$set: infoForUpd}, {upsert: true});
+            let result = await collection.updateOne(query, {$set: req.body}, {upsert: true});
             res.send(result);
         } catch (err) {
             console.log(err);
