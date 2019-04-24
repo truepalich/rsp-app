207b8f01bd4268cc76aa821b0c500fbc114ef168
pavel_lebedev <designer.lebedev@gmail.com>
2019-04-25 00:23:39 +0300

    Update

--- app/js/lists.js
+++ app/js/lists.js
@@ -1,23 +1,25 @@
 const form = $('.js-content');
-console.log('listjs loaded');
-(function () {
-    let formData =
+
+
+$(document).ready(function () {
+  console.log('listjs loaded');
+  let formData =
+    {
+      title: "Title of list",
+      list: [
         {
-            title: "Title of list",
-            list: [
-                {
-                    text: "text of list",
-                    status: true
-                }
-            ],
-            type: "lists"
-        };
+          text: "text of list",
+          status: true
+        }
+      ],
+      type: "lists"
+    };
 
-    const bodyTag = $('body');
+  const bodyTag = $('body');
 
-    function appendForm() {
-        form.empty();
-        form.append(`
+  function appendForm() {
+    form.empty();
+    form.append(`
 <div class="row js-common-section" id="add-list-section" style="display: block">
    <div class="col">
       <h4>Add list</h4>
@@ -38,175 +40,214 @@ console.log('listjs loaded');
       </form>
    </div>
 </div>`);
-    }
+  }
+
+  if (window.location.hash === '#lists') {
+    appendForm()
+  }
 
 
-
-
-    bodyTag.on('click', '#btnGroupAddon', function (e) {
-        e.preventDefault();
-        $(this).parent().append(`<button class="input-group-text" id="removeItem">-</button>`);
-        this.remove();
-        $('.js-form-group').append(`
+  bodyTag.on('click', '#btnGroupAddon', function (e) {
+    e.preventDefault();
+    $(this).parent().append(`<button class="input-group-text" id="removeItem">-</button>`);
+    this.remove();
+    $('.js-form-group').append(`
 <div class="input-group">
     <input type="text" class="form-control list-elem-data" placeholder="Enter text" aria-label="Input group example" aria-describedby="btnGroupAddon">
     <div class="input-group-prepend">
        <button class="input-group-text" id="btnGroupAddon">+</button>
     </div>
 </div>`)
-    });
+  });
 
 
-    bodyTag.on('click', '#removeItem', function (e) {
-        $(this).parents('.input-group').remove()
-    });
+  bodyTag.on('click', '#removeItem', function (e) {
+    $(this).parents('.input-group').remove()
+  });
 
 
-    async function sendFormData() {
-        let list = [];
-        $('.list-elem-data').each(function () {
-            list.push(
-                {
-                    text: $(this).val(),
-                    status: true
-                }
-            )
-        });
-
-        formData =
-            {
-                title: $('#list-title').val(),
-                list: list,
-                type: "lists"
-            };
-
-        $('.js-preloader').show();
-
-        try {
-            await axios.post('http:/localhost:3000/api/lists',formData);
-            window.location.hash = '#';
-            $('.js-preloader').hide()
-        } catch (error) {
-            console.error(error);
+  async function sendFormData() {
+    let list = [];
+    $('.list-elem-data').each(function () {
+      list.push(
+        {
+          text: $(this).val(),
+          status: true
         }
+      )
+    });
+
+    formData =
+      {
+        title: $('#list-title').val(),
+        list: list,
+        type: "lists"
+      };
+
+    $('.js-preloader').show();
+
+    try {
+      await axios.post('http:/localhost:3000/api/lists', formData);
+      window.location.hash = '#';
+      $('.js-preloader').hide()
+    } catch (error) {
+      console.error(error);
+    }
 
     //     axios.post('http:/localhost:3000/api/lists',formData)
-        //       .then(function (res) {
-        //           window.location.hash = '#';
-        //           $('.js-preloader').hide()
-        //          })
+    //       .then(function (res) {
+    //           window.location.hash = '#';
+    //           $('.js-preloader').hide()
+    //          })
 
-    }
+  }
 
 
-    bodyTag.on('click', '.js-submit-list', function (e) {
-        e.preventDefault();
-        checkForm(sendFormData);
+  bodyTag.on('click', '.js-submit-list', function (e) {
+    e.preventDefault();
+    checkForm(sendFormData);
+  });
+
+  function checkForm(callback) {
+    const formControl = $('.form-control');
+    formControl.each(function () {
+      $(this).removeClass('border-danger')
     });
 
-    function checkForm(callback) {
-        const formControl = $('.form-control');
-        formControl.each(function () {
-            $(this).removeClass('border-danger')
-        });
-
-        let empty = formControl.filter(function () {
-            return this.value === ''
-        });
-
-        empty.map(function () {
-            $(this).addClass('border-danger');
-        });
-
-        if (empty.length === 0) {
-            callback();
-        }
-    }
-
-    $(window).on('popstate', function () {
-        if (window.location.hash === '#lists') {
-            appendForm()
-        }
+    let empty = formControl.filter(function () {
+      return this.value === ''
     });
 
-})();
+    empty.map(function () {
+      $(this).addClass('border-danger');
+    });
 
-// <button type="button" class="list-group-item list-group-item-action">Dapibus ac facilisis in</button>
-// data.list[0].text
+    if (empty.length === 0) {
+      callback();
+    }
+  }
 
+  $(window).on('popstate', function () {
+    if (window.location.hash === '#lists') {
+      appendForm()
+    }
+  });
 
-const data = {"_id":{"$oid":"5cbe0c7ee162ec0e1ceeef97"},"title":"Item for test","list":[{"text":"First element","status":true},{"text":"Second element","status":true},{"text":"Third element","status":true},{"text":"Fourth element","status":false},{"text":"Fifth element","status":true}],"type":"lists"}
+})
 
+$(document).ready(function () {
 
-function generateListItem(data){
+  const itemId = window.location.hash.split('/').pop();
+
+  function generateListItem(data) {
     return `
 <ul class="list-group">
-  ${data.list.map((item,i)=>{
+  ${data.list.map((item, i) => {
       return `<li class="list-group-item js-list-item
-                ${data.list[i].status === false ? "js-list-item-inactive" : ""}"
+                ${data.list[i].status === 'false' ? "js-list-item-inactive" : ""}"
                 data-status=${data.list[i].status}>${data.list[i].text}</li>`
     }).join('')}
 </ul>
-<button type="button" class="btn btn-primary btn-lg btn-block js-update-list-item">Save</button>`
-}
+<button type="button" class="btn btn-primary btn-lg btn-block js-update-list-item">Save</button>
+<button type="button" class="btn btn-primary btn-lg btn-block js-back-to-index">Return to homepage</button>`
+  }
 
 
-form.empty();
-let errorCount = 0;
-async function getListItem() {
+  form.empty();
+  let errorCount = 0;
+
+  async function getListItem(id) {
+
     try {
-        // const response = await axios.get('http://localhost:3000/lists/5cbe0c7ee162ec0e1ceeef97');
-        // form.append(generateListItem(response.data[0]))
-        form.append(generateListItem(data))
+      const response = await axios.get(`http://localhost:3000/lists/${id}`);
+      form.empty();
+      form.append(generateListItem(response.data[0]))
     } catch (error) {
-        errorCount++;
-        if (errorCount === 5){
-            return $('.js-error').show()
-        }
-        getListItem();
+      errorCount++;
+      if (errorCount === 5) {
+        return $('.js-error').show()
+      }
+      getListItem(itemId);
     }
-}
-getListItem();
+  }
+
+  if (window.location.hash.includes('#api/lists/')) {
+    if (checkUrl()){
+      getListItem(itemId)
+    }
+  }
+
+  function checkUrl(){
+    if (window.location.hash === '#api/lists/' || window.location.hash === '#api/lists' ){
+      window.location.hash = '#';
+      return false
+    }else {
+      return true
+    }
+  }
 
 
 
-
-$('body').on('click','.js-list-item',function () {
+  $('body').on('click', '.js-list-item', function () {
     // console.log(typeof $(this).data('status'));
     let statusValue = $(this).attr('data-status');
 
-    if (statusValue === 'true'){
-        $(this).addClass('js-list-item-inactive');
-        statusValue = 'false'
-        $(this).attr('data-status', statusValue)
+    if (statusValue === 'true') {
+      $(this).addClass('js-list-item-inactive');
+      statusValue = 'false'
+      $(this).attr('data-status', statusValue)
 
-    } else if (statusValue === 'false'){
-        $(this).removeClass('js-list-item-inactive');
-        statusValue = 'true';
-        $(this).attr('data-status', statusValue)
+    } else if (statusValue === 'false') {
+      $(this).removeClass('js-list-item-inactive');
+      statusValue = 'true';
+      $(this).attr('data-status', statusValue)
     }
 
-});
+  });
+
+  $('body').on('click', '.js-back-to-index', function () {
+    window.location.hash = '#'
+  });
 
 
-$('body').on('click','.js-update-list-item',async function (e) {
+  $('body').on('click', '.js-update-list-item', async function (e) {
     e.preventDefault();
     let data = [];
 
     $('.js-list-item').each(function () {
-        data.push(
-            {
-                text: $(this).text().replace('\n',''),
-                status: $(this).attr('data-status')
-            }
-        )
+      data.push(
+        {
+          text: $(this).text().replace('\n', ''),
+          status: $(this).attr('data-status')
+        }
+      )
     });
 
     try {
-        const response = await axios.put('http://localhost:3000/lists/5cbe0c7ee162ec0e1ceeef97',JSON.stringify(data));
-        window.location.href = "#";
-        console.log(data);
+      await axios.put(`http://localhost:3000/api/lists/${itemId}`, data);
+      console.log(data);
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
+  });
+
+
+
+  $(window).on('popstate',function () {
+    if (window.location.hash.includes('#api/lists/')) {
+      if (checkUrl()){
+        getListItem(itemId)
+      }
+    }
+  })
+
+})
+
+
+
+
+$(window).on('popstate', function () {
+  if (window.location.hash === '' || window.location.hash === '#') {
+    form.empty()
+  }
 });
--- app/lists/editListItem.js
+++ app/lists/editListItem.js
@@ -2,11 +2,12 @@ const ObjectId = require('mongodb').ObjectId;
 
 module.exports = (app, collection) => {
   app.put('/api/lists/:id', (req, res) => {
-    collection.findOneAndUpdate({_id:ObjectId(req.params.id)},{$set:{list:25}}, (err, result) => {
+    collection.findOneAndUpdate({_id:ObjectId(req.params.id)},{$set:{list:req.body}}, (err, result) => {
       if (err) {
         return console.log(err)
       }
       console.log('old item updated');
+      console.log(req.body)
       res.send(`ONE LIST ITEM WAS UPDATED`)
     })
   });
