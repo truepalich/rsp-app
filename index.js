a443530ca4b4f72efc546dd120c7db3bb31bf429
Pasha <designer.lebedev@gmail.com>
2019-05-11 00:19:53 +0300

    Changed port

--- index.js
+++ index.js
@@ -13,7 +13,7 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const db = require('./config/db.js');
